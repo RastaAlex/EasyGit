0975e7b35cf23b9791c9ce50dc190fc01d02e6d9
RastaAlex <Rastaalex86@gmail.com>
2020-07-15 19:53:52 +0300

    new commit

---
+++ bin/easygit.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+const git = require('simple-git/promise');
+const tryToCatch = require('try-to-catch');
+let isProcess;
+
+async function runGit() {
+    isProcess = true;
+    console.log(isProcess);
+    await git().add('easygit.js');
+    console.log('file add');
+    await git().commit('new commit');
+    console.log('file commit');
+    await git().push('origin', 'master');
+    console.log('file push');
+    isProcess = false;
+    console.log(isProcess);
+}
+//main().catch((err) => console.log(err.message));
+
+const watcher = fs.watch('./', async (event) => {
+    if (event === 'change') {
+        await runGit();
+    }
+    const [error] = await tryToCatch(runGit);
+    
+    if (error)
+        console.error(error);
+});
+
+if (isProcess === false) {
+    watcher.close();
+}
